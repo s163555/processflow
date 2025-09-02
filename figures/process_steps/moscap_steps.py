8e993dccd095e6789c0b0befe6fe786d2fb024cc
Jeppe <dem16216syh@gmail.com>
2025-09-02 12:07:34 +0200

    Lift-off process in backside electrode cross-sections

--- figures/process_steps/moscap_steps.py
+++ figures/process_steps/moscap_steps.py
@@ -7,7 +7,7 @@ def draw_step(ax, step_id, step_desc,
               show_oxide=False, show_poly=False, patterned=False,
               poly_doped=False, backside_poly_etched=False,
               backside_oxide_etched=False,
-              show_ti=False, show_al=False, annealed=False):
+              show_ti=False, show_al=False, patterned_backside=False, annealed=False):
 
     # Geometry constants
     W = 8.0
@@ -71,34 +71,39 @@ def draw_step(ax, step_id, step_desc,
             ax.text(W/2, y_poly_top + H_poly/2, label,
                     ha='center', va='center', fontsize=10, color='white')
 
-    # --- Backside Ti ---
-    y_back_metal = -0.1
+# --- Backside Ti ---
     if show_ti:
-        # Position Ti layer below any remaining backside films or directly on substrate
         y_ti = y_poly_back - H_poly_back if show_poly and not backside_poly_etched else y_back
         y_ti = y_ti - H_ox_back if show_oxide and not backside_oxide_etched else y_ti
         
-        ax.add_patch(Rectangle((0, y_ti - H_ti), W, H_ti,
-                               facecolor="#9999cc", edgecolor="black"))
+        if patterned_backside:
+            elec_w = 4.0
+            elec_x = (W - elec_w)/2
+            ax.add_patch(Rectangle((elec_x, y_ti - H_ti), elec_w, H_ti,
+                                facecolor="#9999cc", edgecolor="black"))
+        else:
+            ax.add_patch(Rectangle((0, y_ti - H_ti), W, H_ti,
+                                facecolor="#9999cc", edgecolor="black"))
         ax.text(W/2, y_ti - H_ti/2, "Backside Ti (100 nm)",
                 ha='center', va='center', fontsize=9)
 
     # --- Backside Al ---
     if show_al:
-        # Position Al layer below Ti layer
         y_al = y_ti - H_ti if show_ti else y_back_metal
-        
-        if annealed:
-            ax.add_patch(Rectangle((0, y_al - H_al), W, H_al,
-                                   facecolor="#cccccc", edgecolor="black", hatch="////"))
-            ax.text(W/2, y_al - H_al/2, "Backside Al (400 nm, annealed)",
-                    ha='center', va='center', fontsize=9, 
-                    bbox=dict(facecolor="white", edgecolor="none", alpha=0.7))
+        if patterned_backside:
+            elec_w = 4.0
+            elec_x = (W - elec_w)/2
+            ax.add_patch(Rectangle((elec_x, y_al - H_al), elec_w, H_al,
+                                facecolor="#cccccc", edgecolor="black",
+                                hatch="////" if annealed else None))
         else:
             ax.add_patch(Rectangle((0, y_al - H_al), W, H_al,
-                                   facecolor="#cccccc", edgecolor="black"))
-            ax.text(W/2, y_al - H_al/2, "Backside Al (400 nm)",
-                    ha='center', va='center', fontsize=9)
+                                facecolor="#cccccc", edgecolor="black",
+                                hatch="////" if annealed else None))
+        label = "Backside Al (400 nm, annealed)" if annealed else "Backside Al (400 nm)"
+        ax.text(W/2, y_al - H_al/2, label,
+                ha='center', va='center', fontsize=9,
+                bbox=dict(facecolor="white", edgecolor="none", alpha=0.7) if annealed else None)
 
     # --- Figure limits ---
     top_y = H_sub + H_ox + H_poly
@@ -131,13 +136,13 @@ steps = [
     ("6.6", "Gate poly etch", dict(show_oxide=True, show_poly=True, patterned=True, poly_doped=True, 
                                   backside_poly_etched=True, backside_oxide_etched=True)),
     ("7.5", "Backside Ti deposition", dict(show_oxide=True, show_poly=True, patterned=True, poly_doped=True, 
-                                         backside_poly_etched=True, backside_oxide_etched=True, show_ti=True)),
+                                         backside_poly_etched=True, backside_oxide_etched=True, show_ti=True, patterned_backside=True)),
     ("7.6", "Backside Al deposition", dict(show_oxide=True, show_poly=True, patterned=True, poly_doped=True, 
                                          backside_poly_etched=True, backside_oxide_etched=True, 
-                                         show_ti=True, show_al=True)),
+                                         show_ti=True, show_al=True, patterned_backside=True)),
     ("7.9", "Contact anneal", dict(show_oxide=True, show_poly=True, patterned=True, poly_doped=True, 
                                   backside_poly_etched=True, backside_oxide_etched=True,
-                                  show_ti=True, show_al=True, annealed=True)),
+                                  show_ti=True, show_al=True, patterned_backside=True, annealed=True)),
 ]
 
 # --- Output folder ---
